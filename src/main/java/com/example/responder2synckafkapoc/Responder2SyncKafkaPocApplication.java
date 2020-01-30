169ed04d18b05d7718287bdd34518132b55aaacf
Balaji Ramasamy <cubanhog.royalgamma@gmail.com>
2020-01-31 04:07:13 +0530

    Initial commit

---
+++ src/main/java/com/example/responder2synckafkapoc/Responder2SyncKafkaPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.responder2synckafkapoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Responder2SyncKafkaPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Responder2SyncKafkaPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/responder2synckafkapoc/Responder2SyncKafkaPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.responder2synckafkapoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Responder2SyncKafkaPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
